31a0a1c73ca073f7bd84723b193fbcb1da3f7c95
TaurusDWill <twilliams542@my.gcu.edu>
2024-04-16 19:48:51 -0700

    Lantern-Download

---
+++ Lantern_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,118 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID DOORCREAK = 227958399U;
+        static const AkUniqueID DOOROPEN = 1404805401U;
+        static const AkUniqueID DOORSLAM = 3178299198U;
+        static const AkUniqueID DOORSLAMOUTSIDE = 3269234127U;
+        static const AkUniqueID DOORUNLOCK = 1553818165U;
+        static const AkUniqueID KEYPADFAIL = 2411561249U;
+        static const AkUniqueID KEYPADPRESSED = 2108322741U;
+        static const AkUniqueID KEYPADSUCCESS = 2353527262U;
+        static const AkUniqueID LANTERNPICKUP = 1546523411U;
+        static const AkUniqueID LANTERNTHUD = 2165330516U;
+        static const AkUniqueID LVL5BGM = 3600548152U;
+        static const AkUniqueID LVL5FADEOUT = 1358746344U;
+        static const AkUniqueID PLAY = 1256202815U;
+        static const AkUniqueID STAIRSNEXTLEVEL = 131039866U;
+        static const AkUniqueID START = 1281810935U;
+        static const AkUniqueID WALKINGSTONE = 881533257U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace INGAME
+        {
+            static const AkUniqueID GROUP = 984691642U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID LEVELTRANSITION = 4198606512U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace INGAME
+
+        namespace MAIN
+        {
+            static const AkUniqueID GROUP = 3161908922U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IDELINMAINMENU = 2707144702U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STARTINGGAME = 1300063251U;
+            } // namespace STATE
+        } // namespace MAIN
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CLOVERS = 1394758289U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID NEW_TRIGGER = 4163741908U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DOORCREAK = 227958399U;
+        static const AkUniqueID DOOROPEN = 1404805401U;
+        static const AkUniqueID DOORSLAM = 3178299198U;
+        static const AkUniqueID DOORSLAMOUTSIDE = 3269234127U;
+        static const AkUniqueID DOORUNLOCK = 1553818165U;
+        static const AkUniqueID INGAMEBGM = 1893515252U;
+        static const AkUniqueID KEYPADFAIL = 2411561249U;
+        static const AkUniqueID KEYPADPRESSED = 2108322741U;
+        static const AkUniqueID KEYPADSUCCESS = 2353527262U;
+        static const AkUniqueID LANTERNPICKUP = 1546523411U;
+        static const AkUniqueID LANTERNTHUD = 2165330516U;
+        static const AkUniqueID LVL5BGM = 3600548152U;
+        static const AkUniqueID LVL5FADEOUT = 1358746344U;
+        static const AkUniqueID STAIRSNEXTLEVEL = 131039866U;
+        static const AkUniqueID STARTBUTTON = 1651515387U;
+        static const AkUniqueID TITLEMUSIC = 2191159630U;
+        static const AkUniqueID WALKINGSTONE = 881533257U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
